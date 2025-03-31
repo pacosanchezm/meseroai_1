065eaffa1c48d3cff923620c59059cebfa20fd8d
pacosanchezm <paco.sanchezm@gmail.com>
2025-03-31 22:19:40 +0000

    poc5

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -1,37 +1,37 @@
 import { IMenuItem, ISocials } from "@/types";
 
 export const footerDetails: {
-    subheading: string;
-    quickLinks: IMenuItem[];
-    email: string;
-    telephone: string;
-    socials: ISocials;
+  subheading: string;
+  quickLinks: IMenuItem[];
+  email: string;
+  telephone: string;
+  socials: ISocials;
 } = {
-    subheading: "cambiando la experiencia de tu cliente.",
-    quickLinks: [
-        {
-            text: "Ventajas",
-            url: "#features"
-        },
-        {
-            text: "Precios",
-            url: "#pricing"
-        },
-        {
-            text: "Testimonios",
-            url: "#testimonials"
-        }
-    ],
-    email: 'info@agentia.co',
-    telephone: '(477) 274-0011',
-    socials: {
-        // github: 'https://github.com',
-        // x: 'https://twitter.com/x',
-        twitter: 'https://twitter.com/Twitter',
-        facebook: 'https://facebook.com',
-        // youtube: 'https://youtube.com',
-        linkedin: 'https://www.linkedin.com',
-        // threads: 'https://www.threads.net',
-        instagram: 'https://www.instagram.com',
-    }
-}
+  subheading: "cambiando la experiencia de tu cliente.",
+  quickLinks: [
+    {
+      text: "Ventajas",
+      url: "#features",
+    },
+    {
+      text: "Precios",
+      url: "#pricing",
+    },
+    {
+      text: "Testimonios",
+      url: "#testimonials",
+    },
+  ],
+  email: "info@mesero.ia",
+  telephone: "----",
+  socials: {
+    // github: 'https://github.com',
+    // x: 'https://twitter.com/x',
+    twitter: "https://twitter.com/Twitter",
+    facebook: "https://facebook.com",
+    // youtube: 'https://youtube.com',
+    linkedin: "https://www.linkedin.com",
+    // threads: 'https://www.threads.net',
+    instagram: "https://www.instagram.com",
+  },
+};
--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,6 @@
 export const heroDetails = {
-    heading: 'Tu asistente de IA para tu restaurante',
-    subheading: 'Atiende a tus clientes en whatsapp y escribe el pedido directamente en tu software de restaurante',
-    centerImageSrc: '/images/logohero.png',
-}
+  heading: "El asistente de IA para tu restaurante",
+  subheading:
+    "Atiende a tus clientes en whatsapp y escribe el pedido directamente en tu software de restaurante",
+  centerImageSrc: "/images/logohero.png",
+};
